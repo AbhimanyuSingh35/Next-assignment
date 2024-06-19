c77a35839bed4a07e9137616cc2c3ce12d3adf87
Abhimanyu35 <22dcs001@nith.ac.in>
2024-06-19 17:15:49 +0530

    patial-prerendering

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental : {
+    ppr: 'incremental',
+  },
+  //incremental value allows us adopt PPR for specific routes.
+  //Next, add the experimental_ppr segment config option toyou dashboard layout 
+  
+};
 
 module.exports = nextConfig;
